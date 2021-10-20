f7139705ec0dcdf0fd273cf4abada40b85de3ce1
PVMAIA <pvpaulo16@gmail.com>
2021-10-19 21:30:31 -0300

    My first api node without express

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const UserController = require('./controllers/UserController');
+
+module.exports = [
+   {
+      enpoint: '/users',
+      method: 'GET',
+      handler: UserController.listUsers,
+   },
+   {
+      enpoint: '/users/:id',
+      method: 'GET',
+      handler: UserController.getUserById,
+   },
+   {
+      enpoint: '/users',
+      method: 'POST',
+      handler: UserController.createUser,
+   },
+   {
+      enpoint: '/users/:id',
+      method: 'PUT',
+      handler: UserController.updateUser,
+   },
+   {
+      enpoint: '/users/:id',
+      method: 'DELETE',
+      handler: UserController.deleteUser,
+   },
+];
